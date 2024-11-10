eabcd69cf6c6ac7a88f8747ccd135126e920844a
Nguyen Quang Tuan Phuong <130271080+hayamij@users.noreply.github.com>
2024-11-10 18:56:48 +0700

    Update and rename input.java to frame.java

---
+++ frame.java
@@ -0,0 +1,13 @@
+package ItemFlow;
+import javax.swing.*;
+
+public class frame {
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+    frame.setSize(1280, 720);
+    frame.setTitle("ItemFlow");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.SetLocationRelativeTo(null);
+  }
+}
